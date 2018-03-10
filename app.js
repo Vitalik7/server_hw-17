1a740ed818586c518a5b8b2fd902f5cc01a2a402
Vitaliy <vitaliy@MacBook-Pro-Vitaliy.local>
2018-03-10 17:35:21 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,91 @@
+const app = require('express')()
+const http = require('http').Server(app)
+const bodyParser = require('body-parser')
+const io = require('socket.io')(http)
+const config = require('./config')
+const MessageModel = require('./chat/model')
+const message = require('./chat/chat')
+const user = require('./user/user')
+const passport = require('passport')
+const FacebookStrategy = require('passport-facebook')
+const facebook = config.facebook
+
+require('./db')
+
+http.listen(config.port, () => {
+  console.log(`Server running at port: ${config.port}`)
+})
+
+const transformFacebookProfile = (profile) => ({
+  name: profile.name,
+  avatar: profile.picture.data.url
+})
+
+passport.use(new FacebookStrategy(facebook,
+    // Gets called when user authorizes access to their profile
+    async function (accessToken, refreshToken, profile, done) {
+      done(null, transformFacebookProfile(profile._json))
+    }
+))
+
+// Serialize user into the sessions
+passport.serializeUser((user, done) => done(null, user))
+
+// Deserialize user from the sessions
+passport.deserializeUser((user, done) => done(null, user))
+
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(bodyParser.json())
+app.use('/api', message)
+app.use('/api', user)
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/auth/facebook/callback',
+    passport.authenticate('facebook', { failureRedirect: '/auth/facebook' }),
+    // Redirect user back to the mobile app using Linking with a custom protocol OAuthLogin
+    (req, res) => {
+      res.redirect('OAuthLogin://login?user=' + JSON.stringify(req.user))
+    })
+
+io.on('connection', function (socket) {
+  console.log('a user connected')
+  socket.on('chat message', function (msg) {
+    console.log('incoming msg: ' + JSON.stringify(msg))
+    new MessageModel(msg)
+          .save()
+        .then(message => {
+          io.emit('chat message', message)
+        })
+  })
+
+  socket.on('disconnect', function () {
+    console.log('user disconnected')
+  })
+})
+
+// error handling
+app.use((req, res, next) => {
+  const err = new Error(`Not Found ${req.path}`)
+  err.status = 404
+  next(err)
+})
+app.use((error, req, res, next) => {
+  if (error) {
+    console.log(error)
+    return res.status(400).json({error})
+  }
+  next(error)
+})
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
+  res.render('error', {
+    message: err.message,
+    error: {}
+  })
+})
+
+module.exports = app
---
+++ config.js
@@ -0,0 +1,14 @@
+const host = 'localhost'
+
+// noinspection JSAnnotator
+module.exports = {
+  port: process.env.port || process.env.PORT || '3000',
+  mongoUrl: process.env.MONGODB_URI || `mongodb://lavrenko_777:vitalik231@ds211289.mlab.com:11289/react-native-chat`,
+  facebook: {
+    clientID: '156122841758549',
+    clientSecret: 'ff06acffc684562ab896a64af7bdb083',
+    // callbackURL: 'http://192.168.1.109:3000/auth/facebook/callback',
+    callbackURL: 'https://limitless-gorge-54663.herokuapp.com/auth/facebook/callback',
+    profileFields: ['id', 'name', 'displayName', 'picture', 'email']
+  }
+}
